e021710c125a747a0ce05c4e06cd07d75c32270e
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-22 20:18:22 +0200

    add platform warnings

--- detox/src/expect/index.js
+++ detox/src/expect/index.js
@@ -2,11 +2,19 @@ class Element {
   constructor(matcher) {
     this.matcher = matcher;
   }
+
+  tap() {
+    throw new Error("Not implemented on this platform");
+  }
 }
 
 class Expect {
   constructor(element) {
     this.element = element;
+  }
+
+  toBeVisible() {
+    throw new Error("Not implemented on this platform");
   }
 }
 module.exports = { Element, Expect };
